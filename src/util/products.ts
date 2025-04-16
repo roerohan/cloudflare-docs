8dfbbdb0569e2495db52e88d9637a1e1de102dd2
Kian Newman-Hazel <kian@cloudflare.com>
2025-04-16 21:47:07 +0100

    formatting

--- src/util/products.ts
+++ src/util/products.ts
@@ -5,27 +5,32 @@ export const products = await getCollection("products");
 export const productsByGroup = Object.entries(
 	products
 		.filter((product) => Boolean(product.data.product.group))
-		.reduce((groups, product) => {
-			const primaryGroup = product.data.product.group;
-			const additionalGroups = product.data.product.additional_groups ?? [];
-			const allGroups = [primaryGroup, ...additionalGroups];
+		.reduce(
+			(groups, product) => {
+				const primaryGroup = product.data.product.group;
+				const additionalGroups = product.data.product.additional_groups ?? [];
+				const allGroups = [primaryGroup, ...additionalGroups];
 
-			for (const group of allGroups) {
-				if (!groups[group]) {
-					groups[group] = [];
+				for (const group of allGroups) {
+					if (!groups[group]) {
+						groups[group] = [];
+					}
+					groups[group].push(product);
 				}
-				groups[group].push(product);
-			}
 
-			return groups;
-		}, {} as Record<string, typeof products>),
+				return groups;
+			},
+			{} as Record<string, typeof products>,
+		),
 );
 
 export const groups = [
 	...new Set(
-		products.flatMap((product) => [
-			product.data.product.group,
-			...(product.data.product.additional_groups ?? []),
-		].filter(Boolean)),
+		products.flatMap((product) =>
+			[
+				product.data.product.group,
+				...(product.data.product.additional_groups ?? []),
+			].filter(Boolean),
+		),
 	),
 ].sort();
